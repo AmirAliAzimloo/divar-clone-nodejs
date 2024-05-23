63344dd2d9b3f80faf46faccfa571e9a62b72f42
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-05-23 10:52:20 +0430

    Add error handlers.

--- main.js
+++ main.js
@@ -2,24 +2,37 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const SwaggerConfig = require("./src/config/swagger.config");
+
 const mainRouter = require("./src/app.routes");
+
+const NotFoundHandler = require("./src/common/exception/not-found.handler");
+const AllExceptionHandler = require("./src/common/exception/all-exception.handler");
 
 dotenv.config();
 
 async function main () {
+    //* constants
     const app = express();
     const port = process.env.PORT;
 
+    //* data base connection
     require("./src/config/mongoose.config");
+
+    //* utils
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
+
+    //* ruters
+    app.use(mainRouter);
 
     //* swagger
     SwaggerConfig(app);
 
-    //* ruters
-    app.use(mainRouter);
+    //* error handler
+    NotFoundHandler(app);
+    AllExceptionHandler(app);
     
+    //* server
     app.listen(port, () => {
         console.log(`server: http://localhost:${port}`);
     });
