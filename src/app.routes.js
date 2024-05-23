fcba3864e28e408f0472fd4c84ca688d251497ac
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-05-23 13:18:11 +0430

    Add user controller.

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const {AuthRouter} = require("./modules/auth/auth.routes");
-// const {UserRouter} = require("./modules/user/user.routes");
+const {UserRouter} = require("./modules/user/user.routes");
 // const {CategoryRouter} = require("./modules/category/category.routes");
 // const {OptionRoutes} = require("./modules/option/option.routes");
 // const {PostRouter} = require("./modules/post/post.routes");
@@ -8,7 +8,7 @@ const {AuthRouter} = require("./modules/auth/auth.routes");
 // const postController = require("./modules/post/post.controller");
 const mainRouter = Router();
 mainRouter.use("/auth", AuthRouter);
-// mainRouter.use("/user", UserRouter); 
+mainRouter.use("/user", UserRouter); 
 // mainRouter.use("/category", CategoryRouter);
 // mainRouter.use("/option", OptionRoutes);
 // mainRouter.use("/post", PostRouter);
