cc8cbef0be8ef769ad4beec4b7549b31e0254936
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-05-24 04:50:47 +0430

    Complete the project.

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const expressEjsLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const moment = require("jalali-moment");
+const methodOverride = require("method-override");
+
 
 const SwaggerConfig = require("./src/config/swagger.config");
-
 const mainRouter = require("./src/app.routes");
-
 const NotFoundHandler = require("./src/common/exception/not-found.handler");
 const AllExceptionHandler = require("./src/common/exception/all-exception.handler");
-const cookieParser = require("cookie-parser");
 
 dotenv.config();
 
@@ -26,8 +28,20 @@ async function main () {
     //* cookie settings
     app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 
+    //* template engine
+    app.use(express.static("public"));
+    app.use(expressEjsLayouts);
+    app.use(methodOverride('_method'));
+    app.set("view engine", "ejs");
+    app.set("layout", "./layouts/panel/main.ejs");
+    app.set("layout extractScripts", true);
+    app.set("layout extractStyles", true);
+
     //* ruters
     app.use(mainRouter);
+
+    //* date & hour
+    app.locals.moment = moment;
 
     //* swagger
     SwaggerConfig(app);
